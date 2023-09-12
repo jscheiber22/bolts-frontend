14ab221c90087109cff27d0e1e749b2d3e9f0386
James Scheiber <jscheiber22@gmail.com>
2023-09-11 17:34:37 -0700

    one more quick ui update to make sure i reuse the same types of text throughou the application

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,8 +1,11 @@
+import { Typography } from "@mui/material";
 import React from "react";
 
 const Settings = () => {
     return (
-        <div>Settings :)</div>
+        <div style={{margin: "3% 7.5% 0% 7.5%"}}>
+            <Typography variant="h2" style={{marginBottom: "3%"}} fontFamily={"inherit"}><strong>Settings :)</strong></Typography>
+        </div>
     )
 }
 
