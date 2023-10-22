d766549d980eabff9b4ef1671e1614a0ce4bdc60
James Scheiber <jscheiber22@gmail.com>
2023-10-21 20:00:45 -0700

    made the projects page data dynamic, next is making it possible to add new data

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,59 +1,46 @@
 import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Projects = () => {
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        axios("http://localhost:5000/api/projects")
+        .then(response => {
+            setProjects(response.data);
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error("error getting data: ", error);
+        })
+    }, []);
+
+
+
     return (
         <div style={{margin: "3% 7.5% 0% 7.5%"}}>
             <Typography variant="h2" style={{marginBottom: "3%"}} fontFamily={"inherit"}><strong>Projects :)</strong></Typography>
             <Grid container spacing={2}>
-                <Grid item xs={6}>
-                    <Card>
-                        <CardMedia
-                            sx={{ height: 140 }}
-                            image="img/car.jpg"
-                            title="car 1"
-                        />
-                        <CardContent>
-                            <Typography variant="h3">car 1</Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item xs={6}>
-                    <Card>
-                        <CardMedia
-                            sx={{ height: 140 }}
-                            image="img/car.jpg"
-                            title="car 2"
-                        />
-                        <CardContent>
-                            <Typography variant="h3">car 2</Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item xs={6}>
-                    <Card>
-                        <CardMedia
-                            sx={{ height: 140 }}
-                            image="img/car.jpg"
-                            title="Car 3"
-                        />
-                        <CardContent>
-                            <Typography variant="h3">car 3</Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item xs={6}>
-                    <Card>
-                        <CardMedia
-                            sx={{ height: 140 }}
-                            image="img/car.jpg"
-                            title="car 4"
-                        />
-                        <CardContent>
-                            <Typography variant="h3">car 4</Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
+                { projects ?
+                    projects.map(project => 
+                        <Grid item xs={6} key={project[0]}>
+                            <Card>
+                                <CardMedia
+                                    sx={{ height: 140 }}
+                                    image={project[5]}
+                                    title={project[1] + " " + project[2]}
+                                />
+                                <CardContent>
+                                    <Typography variant="h3">{project[3] + " " + project[1] + " " + project[2]}</Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    )
+                  :
+                  <p> Loading... </p>
+                }
             </Grid>
         </div>
     )
